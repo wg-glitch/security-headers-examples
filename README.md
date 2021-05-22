3e8d5220cd980c08d9adf7a990849b0d72947e1a
Will Gregg <will.gregg@substrakt.com>
2021-05-22 17:44:52 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Security Headers Examples
+A few boilerplate examples of setting security headers in your application.
+
+These examples are not meant as a one size fits all but a good starting point to get up and running with security headers and start applying them in your projects. I will be adding to these examples as time goes on with new variations and in different programming languages.
+
+## Current Examples
+- .htaccess for Apache configurations
+- JS file for AWS LambdaEdge
---
+++ basic-headers.js
@@ -0,0 +1,42 @@
+/**
+ * LAMBDA EDGE ORIGIN OR VIEWER RESPONSE
+ * @param {*} event 
+ * @param {*} context 
+ * @param {*} callback 
+ */
+ exports.handler = (event, context, callback) => {
+  let response = event.Records[0].cf.response;
+
+  // TRANSPORT SECURITY
+  response.headers['strict-transport-security'] = [{
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubdomains; preload'
+  }];
+  // CONTENT SECURITY POLICY
+  response.headers['content-security-policy'] = [{
+    key: 'Content-Security-Policy',
+    value: "default-src 'self'"
+  }];
+  // CONTENT TYPE
+  response.headers['x-content-type-options'] = [{
+    key: 'X-Content-Type-Options', value: 'nosniff'
+  }];
+  // X-FRAME OPTIONS
+  response.headers['x-frame-options'] = [{
+    key: 'X-Frame-Options', value: 'DENY'
+  }];
+  // XSS PROTECTION
+  response.headers['x-xss-protection'] = [{
+    key: 'X-XSS-Protection', value: '1; mode=block'
+  }];
+  // REFERRER POLICY
+  response.headers['referrer-policy'] = [{
+    key: 'Referrer-Policy', value: 'same-origin'
+  }];
+  // PERMISSIONS POLICY
+  response.headers['permissions-policy'] = [{
+    key: 'Permissions-Policy', value: "camera=(), microphone=()"
+  }];
+  
+  callback(null, response);
+};
